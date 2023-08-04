ea292f0c04327c2bb351c3de8fb2f1514e1f76a5
Tier4Guan <muhua.guan@tier4.jp>
2023-08-04 20:16:53 +0900

    feat: remove unneeded parts in node file and create header file
    Signed-off-by: Tier4Guan <muhua.guan@tier4.jp>

--- planning/behavior_msg_converter/src/behavior_msg_converter_node.cpp
+++ planning/behavior_msg_converter/src/behavior_msg_converter_node.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Tier IV, Inc. (?)
+// Copyright 2023 Tier IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//copied from behavior_velocity_planner
-//Check whether a header is important, by deleting it and see if error.
+// copied from behavior_velocity_planner
+// Check whether a header is important, by deleting it and see if error.
+#include "behavior_msg_converter_node.hpp"
+
 #include <behavior_velocity_planner_common/utilization/path_utilization.hpp>
 #include <lanelet2_extension/utility/message_conversion.hpp>
 #include <tier4_autoware_utils/ros/wait_for_param.hpp>
@@ -35,7 +37,6 @@
 #include <memory>
 #include <vector>
 
-
 // Converting PathWitLaneID to Path
 namespace behavior_msg_converter
 {
@@ -53,8 +54,7 @@ autoware_auto_planning_msgs::msg::Path to_path(
 }
 }  // namespace
 
-
-//Subscriber and publisher
+// Subscriber and publisher
 BehaviorMsgConverterNode::BehaviorMsgConverterNode(const rclcpp::NodeOptions & node_options)
 : Node("behavior_msg_converter_node", node_options),
   tf_buffer_(this->get_clock()),
@@ -67,31 +67,23 @@ BehaviorMsgConverterNode::BehaviorMsgConverterNode(const rclcpp::NodeOptions & n
     this->create_subscription<autoware_auto_planning_msgs::msg::PathWithLaneId>(
       "~/input/path_with_lane_id", 1, std::bind(&BehaviorMsgConverterNode::onTrigger, this, _1),
       createSubscriptionOptions(this));
-      
+
   // Publishers
   path_pub_ = this->create_publisher<autoware_auto_planning_msgs::msg::Path>("~/output/path", 1);
 }
-
 
 // Callback
 void BehaviorVelocityPlannerNode::onTrigger(
   const autoware_auto_planning_msgs::msg::PathWithLaneId::ConstSharedPtr input_path_msg)
 {
-  std::unique_lock<std::mutex> lk(mutex_);
-  
-  if (!isDataReady(planner_data_, *get_clock())) {
-    return;
-  }
-  
+
   if (input_path_msg->points.empty()) {
     return;
   }
-  
+
   const autoware_auto_planning_msgs::msg::Path output_path_msg =
     generatePath(input_path_msg, planner_data_);
-    
-  lk.unlock();
-    
+
   path_pub_->publish(output_path_msg);
 }
 
@@ -100,32 +92,15 @@ autoware_auto_planning_msgs::msg::Path BehaviorVelocityPlannerNode::generatePath
   const PlannerData & planner_data)
 {
   autoware_auto_planning_msgs::msg::Path output_path_msg;
-  
-  
-  // Copied from behavior velocity planner. Remember to fix this with behavior velocity planner.
-  // TODO(someone): support backward path
-  const auto is_driving_forward = motion_utils::isDrivingForward(input_path_msg->points);
-  is_driving_forward_ = is_driving_forward ? is_driving_forward.get() : is_driving_forward_;
-  if (!is_driving_forward_) {
-    RCLCPP_WARN_THROTTLE(
-      get_logger(), *get_clock(), 3000,
-      "Backward path is NOT supported. just converting path_with_lane_id to path");
+
+  // Copied from behavior velocity planner.  Remember to fix this along with behavior velocity planner.
+  // TODO(someone): This must be updated in each scene module, but copy from input message for now.
     output_path_msg = to_path(*input_path_msg);
     output_path_msg.header.frame_id = "map";
     output_path_msg.header.stamp = this->now();
     output_path_msg.left_bound = input_path_msg->left_bound;
     output_path_msg.right_bound = input_path_msg->right_bound;
     return output_path_msg;
-  }
-  
-  
-  // Copied from behavior velocity planner.
-  // TODO(someone): This must be updated in each scene module, but copy from input message for now.
-  output_path_msg.left_bound = input_path_msg->left_bound;
-  output_path_msg.right_bound = input_path_msg->right_bound;
-
-  return output_path_msg;
-  //Why return 2 times? Is the one for only forward situation necessary?
 }
 }  // namespace behavior_msg_converter
 
